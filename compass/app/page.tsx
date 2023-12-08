1cf48598e6bd23c167354a9516d50df88f7f34c1
George Harris <gsh@ad.unc.edu>
2023-12-08 11:31:14 -0500

    Committing before break

--- compass/app/page.tsx
+++ compass/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex h-max flex-col items-center pt-5">
       <img
         src="/placeholder.png"
-        className="fixed left-5 top-5 w-32 h-32"
+        className="fixed left-5 top-5 w-32 h-32 border rounded-md"
       ></img>
       <div className="justify-self-start self-start pl-48 text-4xl font-extrabold">
         Compass Center Advocate Landing Page
