0a3d4d2d6c7b9b0eb40a6cef897cfaed321ec252
anikaahmed114 <aahm@unc.edu>
2024-03-02 16:16:55 -0500

    added comments

--- compass/app/auth/forgot_password/page.tsx
+++ compass/app/auth/forgot_password/page.tsx
@@ -6,14 +6,18 @@ import Paper from '@/components/auth/Paper';
 import { useState, useEffect } from 'react';
 
 function isValidEmail(email: string) {
+    //check email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (email.trim() === '') {
+        //check if input is empty
         return 'Email cannot be empty';
     } else if (!emailRegex.test(email)) {
+        //check for incorrect email format
         return 'Invalid email format';
     }
     else{
+        //return message for no error in input
         return 'If your email exists in the database, you should receive a password reset in your inbox.'
     }
 
@@ -25,8 +29,10 @@ export default function Page()  {
     const [emailError, setEmailError] = useState('');
 
     useEffect(() => {
+        //when email string inputted it is checked in isValidEmail
         const error = isValidEmail(confirmEmail);
         setEmailError(error);
+        //button is disabled if any error is detected
         setIsButtonDisabled(error !== null && !error.includes('exists in the database'));
     }, [confirmEmail]);
 
@@ -40,6 +46,7 @@ export default function Page()  {
                         <Input type='email'
                         title="Enter your email address"
                         placeholder="janedoe@gmail.com"
+                        //setting a placeholder in the email input box
                         value={confirmEmail}
                         icon={'EmailInputIcon'}
                         onChange={(e) => {
@@ -49,10 +56,11 @@ export default function Page()  {
                         <p className={`mt-2 ${emailError.includes('exists in the database') ? 'text-green-500' : 'text-red-500'}`}>
                             {emailError}
                             </p>
+                            //if email does not meet certain criteria, then issue an error
                             )}
                     </div>
                     <div className="flex flex-col items-left space-y-4">
-                        <InlineLink href="/login">
+                        <InlineLink href= "/auth/login"> {/* link back to login page */}
                             Back to Sign In
                         </InlineLink>
                         <Button type="submit" disabled={isButtonDisabled}>
