1f4e27297939a5306721b3106a2acd5d32c7c414
Meliora Ho <nho10@fordham.edu>
2024-03-23 15:53:12 +0000

    FIXED AUTH PAGE

--- compass/app/auth/layout.tsx
+++ compass/app/auth/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <Paper>
-                <form  className="mb-0 m-auto mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8 bg-white max-w-xl">
+                <form  className="mb-0 m-auto mt-6 space-y-4 border border-gray-200 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8 bg-white max-w-xl">
             {children}
          </form>
          <p className="text-center mt-6 text-gray-500 text-xs">
