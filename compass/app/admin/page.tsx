eb8c7e8f2cb187631bb09de936106f2749954926
Andy Chan (12beesinatrenchcoat) <andy@12beesinatrenchco.at>
2024-03-26 15:55:29 -0400

    Rewrite table with TanStack Table
    Not sure if this is the direction we want to go in, but...

--- compass/app/admin/page.tsx
+++ compass/app/admin/page.tsx
@@ -1,28 +1,83 @@
 "use client";
-import Table from "@/components/Table";
+// import Table from "@/components/Table";
+import { ColumnDef, createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { useState } from "react";
+import usersImport from "./users.json";
+
+const usersExample = usersImport as unknown as User[];
+
+type User = {
+  id: number;
+  created_at: any;
+  username: string;
+  role: "ADMIN" | "EMPLOYEE" | "VOLUNTEER";
+  email: string;
+  program: "DOMESTIC" | "ECONOMIC" | "COMMUNITY";
+  experience: number;
+  group?: string;
+}
 
 // TODO: Example data. Remove later.
-const headersExample = ["username", "email", "role", "password"];
-const dataExample = [
-  {id: 1, username: "example", email: "example@gmail.com", role: "role"},
-  {id: 3, username: "erhgoejnrgexample", email: "example@gmail.com", role: "role"},
-  {id: 2, username: "another one", email: "example@gmail.com", role: "role"},
-  {id: 4, username: "examgoiadnfgople", email: "example@gmail.com", role: "role"},
-  {id: 5, username: "edfhgiebroxample", email: "example@gmail.com", role: "role"},
-  {id: 6, username: "examlsdfkjg", email: "example@gmail.com", role: "role"},
-  {id: 7, username: "exkkkample", email: "example@gmail.com", role: "role"},
-]
 
 export default function Page() {
-  const [data, setData] = useState({
-    headers: [...headersExample],
-    data: [...dataExample],
+  const columnHelper = createColumnHelper<User>();
+
+  const columns = [
+    columnHelper.accessor("username", {
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor("role", {
+      cell: info => info.renderValue(),
+    }),
+    columnHelper.accessor("email", {
+      cell: info => info.renderValue(),
+    }),
+    columnHelper.accessor("program", {
+      cell: info => info.renderValue(),
+    })
+  ];
+
+  const [data, setData] = useState<User[]>([...usersExample]);
+
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
   });
 
   return (
     <>
-      <Table headersData={data.headers} data={data.data} />
+      <h1>This is example data!!</h1>
+
+      <table>
+        <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>);
 
 }
