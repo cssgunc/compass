a59f9d784a1e242366c2f2a32b50132e0ffa0de3
Andy Chan (12beesinatrenchcoat) <andy@12beesinatrenchco.at>
2024-03-31 00:32:25 -0400

    Initial implementation of options menu

--- compass/components/Table/Index.tsx
+++ compass/components/Table/Index.tsx
@@ -9,9 +9,10 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { useState } from "react";
-import { RowAction } from "./RowAction";
+import { RowOptionMenu } from "./RowOptionMenu";
 import { RowOpenAction } from "./RowOpenAction";
 import { TableAction } from "./TableAction";
+import { Bars2Icon, AtSymbolIcon, HashtagIcon, ArrowDownCircleIcon } from "@heroicons/react/24/solid";
 
 const usersExample = usersImport as unknown as User[];
 
@@ -31,17 +32,24 @@ export const Table = () => {
   const columnHelper = createColumnHelper<User>();
 
   const columns = [
+    columnHelper.display({
+      id: "options",
+      cell: props => <RowOptionMenu />
+    }),
     columnHelper.accessor("username", {
-      cell: (info) => <RowAction title={info.getValue()} />,
+      header: () => <><Bars2Icon className="inline align-top h-4" /> Username</>,
       cell: (info) => <RowOpenAction title={info.getValue()} />,
     }),
     columnHelper.accessor("role", {
+      header: () => <><ArrowDownCircleIcon className="inline align-top h-4" /> Role</>,
       cell: (info) => info.renderValue(),
     }),
     columnHelper.accessor("email", {
+      header: () => <><AtSymbolIcon className="inline align-top h-4" /> Email</>,
       cell: (info) => info.renderValue(),
     }),
     columnHelper.accessor("program", {
+      header: () => <><ArrowDownCircleIcon className="inline align-top h-4" /> Program</>,
       cell: (info) => info.renderValue(),
     }),
   ];
@@ -54,40 +62,53 @@ export const Table = () => {
     getCoreRowModel: getCoreRowModel(),
   });
 
-  return(
+  return (
     <div className="flex flex-col">
-    <div className="flex flex-row justify-end">
-    <TableAction />
+      <div className="flex flex-row justify-end">
+        <TableAction />
+      </div>
+      <table className="w-full text-xs text-left rtl:text-right">
+        <thead className="text-xs text-gray-500 capitalize">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header, i) => (
+                <th
+                  scope="col"
+                  className={
+                    "p-2 border-gray-200 border-y font-medium "
+                    + ((1 < i && i < columns.length - 1) ? "border-x" : "")
+                  }
+                  key={header.id}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="">
+          {table.getRowModel().rows.map((row) => (
+            <tr className="text-gray-800 border-y lowercase hover:bg-gray-50" key={row.id}>
+              {row.getVisibleCells().map((cell, i) => (
+                <td
+                  className={
+                    "p-2 "
+                    + ((1 < i && i < columns.length - 1) ? "border-x" : "")
+                    + ((i === 0) ? "text-center px-0" : "")
+                  }
+                  key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    <table className="w-full text-xs text-left rtl:text-right">
-          <thead className="text-xs text-gray-500 capitalize">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th scope="col" className="p-3 border-gray-200 border-y font-medium" key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody className="">
-            {table.getRowModel().rows.map((row) => (
-              <tr className="text-gray-800 border-y lowercase hover:bg-gray-50" key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td className="py-2 px-2" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        </div>
   )
 }
