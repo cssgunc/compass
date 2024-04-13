1a04ca7185716a6ba8408845a8f6987810c64009
Aidan Kim <aidankim@ad.unc.edu>
2024-04-13 15:26:36 -0400

    working with importing fixtures

--- backend/test/services/fixtures.py
+++ backend/test/services/fixtures.py
@@ -2,6 +2,6 @@ import pytest
 from sqlalchemy.orm import Session
 from backend.services.service import ServiceService
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def service_svc(session: Session):
     return ServiceService(session)
--- backend/test/services/service_test.py
+++ backend/test/services/service_test.py
@@ -1,4 +1,3 @@
-from unittest.mock import create_autospec
 import pytest
 
 from backend.models.service_model import Service
@@ -6,8 +5,8 @@ from backend.entities.service_entity import ServiceEntity
 from backend.enum_for_models import ProgramTypeEnum
 from backend.services.service import ServiceService
 from backend.services.exceptions import ServiceNotFoundException
-from backend.test.services import service_data
-from backend.test.services.fixtures import service_svc
+from ..services import service_data
+
 
 def test_list(service_svc: ServiceService):
     service = service_svc.get_all()
