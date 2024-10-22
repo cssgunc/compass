722f61364eac3d5a9790167464680354321dc98a
Andy Chan <andy@12beesinatrenchco.at>
2024-10-22 00:17:34 -0400

    Move sidebar open button into Sidebar.tsx, apply to other pages
    Significantly reduces boilerplate of sidebar on pages where it is used.

--- compass/components/Sidebar/Sidebar.tsx
+++ compass/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
     HomeIcon,
     ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
     BookmarkIcon,
     ClipboardIcon,
     BookOpenIcon,
@@ -26,67 +27,86 @@ const Sidebar: React.FC<SidebarProps> = ({
     isAdmin: admin,
 }) => {
     return (
-        <div className={"fixed left-0 w-64 h-full border border-gray-200 bg-gray-50 px-4 "
-            + (isSidebarOpen ? "translate-x-0" : "-translate-x-full opacity-25")
-            + " transition duration-300 ease-out"}>
-        {/* button to close sidebar  */}
-            <div className="flex justify-end">
-                <button
-                    onClick={() => setIsSidebarOpen(false)}
-                    className="py-2 text-gray-500 hover:text-gray-800"
-                    aria-label="Close sidebar"
-                >
-                    <ChevronDoubleLeftIcon className="h-5 w-5" />
-                </button>
-            </div>
-            <div className="flex flex-col space-y-8">
-                {/* user + logout button  */}
-                <div className="flex items-center p-4 space-x-2 border border-gray-200 rounded-md ">
-                    <UserProfile name={name} email={email} />
+        <>
+            {/* Button to open the sidebar. */}
+            <button
+                onClick={() => setIsSidebarOpen(true)}
+                className={`fixed z-20 p-2 text-gray-500 hover:text-gray-800 left-0`}
+                aria-label={"Open sidebar"}
+            >
+                {!isSidebarOpen && (
+                    <ChevronDoubleRightIcon className="h-5 w-5" />
+                )}
+            </button>
+            {/* The sidebar itself. */}
+            <div
+                className={
+                    "fixed left-0 w-64 h-full border border-gray-200 bg-gray-50 px-4 " +
+                    (isSidebarOpen
+                        ? "translate-x-0"
+                        : "-translate-x-full opacity-25") +
+                    " transition duration-300 ease-in-out"
+                }
+            >
+                {/* Button to close sidebar  */}
+                <div className="flex justify-end">
+                    <button
+                        onClick={() => setIsSidebarOpen(false)}
+                        className="py-2 text-gray-500 hover:text-gray-800"
+                        aria-label="Close sidebar"
+                    >
+                        <ChevronDoubleLeftIcon className="h-5 w-5" />
+                    </button>
                 </div>
-                {/* navigation menu  */}
-                <div className="flex flex-col space-y-2">
-                    <h4 className="text-xs font-semibold text-gray-500">
-                        Pages
-                    </h4>
-                    <nav className="flex flex-col">
-                        {admin && (
-                            <SidebarItem
-                                icon={<LockClosedIcon />}
-                                text="Admin"
-                                active={true}
-                                redirect="/admin"
-                            />
-                        )}
+                <div className="flex flex-col space-y-8">
+                    {/* user + logout button  */}
+                    <div className="flex items-center p-4 space-x-2 border border-gray-200 rounded-md ">
+                        <UserProfile name={name} email={email} />
+                    </div>
+                    {/* navigation menu  */}
+                    <div className="flex flex-col space-y-2">
+                        <h4 className="text-xs font-semibold text-gray-500">
+                            Pages
+                        </h4>
+                        <nav className="flex flex-col">
+                            {admin && (
+                                <SidebarItem
+                                    icon={<LockClosedIcon />}
+                                    text="Admin"
+                                    active={true}
+                                    redirect="/admin"
+                                />
+                            )}
 
-                        <SidebarItem
-                            icon={<HomeIcon />}
-                            text="Home"
-                            active={true}
-                            redirect="/home"
-                        />
-                        <SidebarItem
-                            icon={<BookmarkIcon />}
-                            text="Resources"
-                            active={true}
-                            redirect="/resource"
-                        />
-                        <SidebarItem
-                            icon={<ClipboardIcon />}
-                            text="Services"
-                            active={true}
-                            redirect="/service"
-                        />
-                        <SidebarItem
-                            icon={<BookOpenIcon />}
-                            text="Training Manuals"
-                            active={true}
-                            redirect="/training-manuals"
-                        />
-                    </nav>
+                            <SidebarItem
+                                icon={<HomeIcon />}
+                                text="Home"
+                                active={true}
+                                redirect="/home"
+                            />
+                            <SidebarItem
+                                icon={<BookmarkIcon />}
+                                text="Resources"
+                                active={true}
+                                redirect="/resource"
+                            />
+                            <SidebarItem
+                                icon={<ClipboardIcon />}
+                                text="Services"
+                                active={true}
+                                redirect="/service"
+                            />
+                            <SidebarItem
+                                icon={<BookOpenIcon />}
+                                text="Training Manuals"
+                                active={true}
+                                redirect="/training-manuals"
+                            />
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
