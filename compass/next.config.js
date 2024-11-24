259fa9546d60b6419607c63e6841f1104ad568c3
Andy Chan (12beesinatrenchcoat) <andy@12beesinatrenchco.at>
2024-11-23 22:26:02 -0500

    Replace images.domain with images.remotePatterns
    images.domains is deprecated as of Next 14.

--- compass/next.config.js
+++ compass/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["notioly.com"],
+        remotePatterns: [
+            {
+                hostname: "notioly.com",
+            },
+        ],
     },
 };
 
